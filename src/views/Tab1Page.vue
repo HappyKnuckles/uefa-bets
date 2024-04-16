5401d9dc1dff93da92371e33a5ebc3169f4c67a3
Nicolas Hoffmann <hoffmannni92690@th-nuernberg.de>
2024-04-17 00:26:29 +0200

    Add api + tes t

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,22 +2,50 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Communities</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+    <ion-content>
+      <div>
+        <ul>
+          <li v-for="item in community" :key="item.id">{{ item.communityName }}</li>
+        </ul>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'; // Import necessary Vue functions
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+
+import { Configuration, CommunityApi } from '@/generated'; // Import the CommunityApi class and Configuration
+
+const community = ref([]);
+
+// Create a configuration object with the base URL set to https://localhost:44320
+const configuration = new Configuration({ basePath: 'https://localhost:44320' });
+
+// Create an instance of CommunityApi with the configuration
+const communityApi = new CommunityApi(configuration);
+
+// Fetch community data when the component is mounted
+onMounted(async () => {
+  try {
+    // Call the specific API function to get community data
+    const response = await communityApi.apiCommunityGet();
+    const data = await response.data;
+    // Update the community data
+    community.value = data;
+    console.log(community.value)
+  } catch (error) {
+    console.error('Error fetching community data:', error);
+  }
+});
 </script>
+
+
+
+<style scoped>
+/* Add custom styles if needed */
+</style>
